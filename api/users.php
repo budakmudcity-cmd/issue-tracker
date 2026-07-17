5288e3435a019df65a66b72715d9c232a7c9bf9b
budakmudcity-cmd <budakmudcity@gmail.com>
2026-07-17 15:27:11 +0800

    Delete api directory

--- api/users.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php';
-requireAuth();
-
-$result = supabaseRequest('GET', 'users?select=username');
-http_response_code($result['code']);
-echo json_encode($result['data'] ?? []);
